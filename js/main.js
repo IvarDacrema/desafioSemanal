4b2f8e6cf037c55116a1a826ef98362cad68d3c0
Ivar Maximiliano Dacrema <ivar.m.dacrema@hotmail.com>
2022-09-14 12:22:48 -0300

    reordenamiento del codigo

--- js/main.js
+++ js/main.js
@@ -21,92 +21,6 @@ function corroborarVacio(items){
     }
 }
 
-//creamos el cliente
-function crearCliente(){
-    let usuario = prompt("Introduzca un Usuario");
-    corroborarVacio(usuario);
-    let contrasenha = prompt("Introduzca su Contraseña");
-    let bloqueo = crearContrasenha(contrasenha);
-    if(bloqueo != "bloqueado34785"){//para eviar que el programa siga creando el usuario sin la contraseña no se realiza bien.
-    let edad = parseInt(prompt("Introduzca su Edad"));
-    corroborarVacio(edad);
-    let nDocumento = parseInt(prompt("Introduzca su Numero de Documento"));
-    corroborarVacio(nDocumento);
-    let salario = parseInt(prompt("Introduzca su sueldo mensual"))
-    corroborarVacio(salario);
-    usuario = new Cliente(clientes.length, usuario, edad, nDocumento, salario, contrasenha, 0);
-    alert("Usuario creado correctamente");
-    clientes.push(usuario)
-    } else {
-        alert("Cliente no Creado")
-    }
-}
-
-//función crear contraseña
-function crearContrasenha(contrasenha){
-    this.contrasenha = contrasenha;
-    let errores = 0;
-    corroborarVacio(contrasenha);
-    let corroborarContra = prompt("Repita la contraseña");
-    while(contrasenha!=corroborarContra && errores<2 && corroborarContra==""){
-        alert("Las contraseñas no coinciden");
-        corroborarContra = prompt("Repita la contraseña");
-        errores++;
-    }
-    if(errores>=2){
-        alert("No puedes crear tu usuario --> contreñas no coinciden")
-        return contrasenha = "bloqueado34785";//esto evita que el usuario se cree aunque no hagan bien la contraseña
-    }         
-}
-
-//funcion de iniciar sesion
-function iniciarSesion(clientes){
-    let usuarioSesion = prompt("Introduzca su usuario");
-    let usuarioEncontrado = clientes.find((cliente) => cliente.nombre.toLowerCase()==usuarioSesion.toLowerCase())
-    if(usuarioEncontrado==undefined){
-        alert("No esta registrado en nuestra base de datos.\nGracias por su Visita")
-    } else {
-        alert("Usuario Correcto");
-        corroborarContraSesion(usuarioEncontrado);//corrabora la contraseña en el inicio de sesion
-        function corroborarContraSesion(){
-            let intentosFallidos =0;
-            let contrasenhaSesion = prompt("Introduzca su Contraseña")
-            const contrasenhaOk = (contrasenhaSesion===usuarioEncontrado.contrasenha)
-            if(contrasenhaOk){
-                alert("Contraseña Correcta")
-                menuOpciones()
-            } else {
-                while(contrasenhaSesion != usuarioEncontrado.contrasenha && intentosFallidos<2){
-                    alert("Contraseña no coinciden");
-                    contrasenhaSesion = prompt("Introduzca su Contraseña");
-                    intentosFallidos++;
-                }
-            }
-        }
-    }
-}
-
-function menuOpciones(){
-    let menu = prompt("1. Prestamo\n2. Inversiones(Todavia no esta programado)")
-    if(menu==1){
-        pedidoPrestamo();
-    }
-}
-
-function pedidoPrestamo(){
-    let cantidadPrestamo = parseFloat(prompt("¿Cuanto es el dinero que usted quiere recibir?"));
-    controlCero(cantidadPrestamo);
-    alert("¿En cuantas cuota usted quiere devolver el prestamo?");
-    let cuotas = parseInt(prompt("Las opciones son 3, 6, 12, 24 y 48 cuotas"));
-    controlCuotas(cuotas);
-    let cantidadIntereses = eleccionInteres(cuotas);
-    let intereses = calculoItereses(cantidadPrestamo, cantidadIntereses);
-    let valorFinalPrestamo = sumaDineroFinal(cantidadPrestamo, intereses);
-    let valorCuota = calculoCuota(valorFinalPrestamo, cuotas);
-    alert("Tu Prestamo a sido Calculado");
-    muestra(cantidadPrestamo, intereses, cuotas, valorCuota)
-}
-
 //funcion que verifica que el pedido de prestamo es mayor a cero
 function controlCero(cantidadPrestamo){
     while(cantidadPrestamo <= 0){
@@ -155,6 +69,28 @@ function calculoCuota(valorFinalPrestamo, cuotas){
     return valorFinalPrestamo/cuotas;
 }
 
+//muestra lista de los clientes
+function muestraClientes(clientes){
+    clientes.forEach((cliente) => {console.log(`Nombre: ${cliente.nombre} y ID: ${cliente.id}`)});
+}
+
+//función crear contraseña
+function crearContrasenha(contrasenha){
+    this.contrasenha = contrasenha;
+    let errores = 0;
+    corroborarVacio(contrasenha);
+    let corroborarContra = prompt("Repita la contraseña");
+    while(contrasenha!=corroborarContra && errores<2 && corroborarContra==""){
+        alert("Las contraseñas no coinciden");
+        corroborarContra = prompt("Repita la contraseña");
+        errores++;
+    }
+    if(errores>=2){
+        alert("No puedes crear tu usuario --> contreñas no coinciden")
+        return contrasenha = "bloqueado34785";//esto evita que el usuario se cree aunque no hagan bien la contraseña
+    }         
+}
+
 function aceptarTrato(){
     let validacion = prompt("¿Acepta el prestamo? Si/No");
     if(validacion.toLowerCase()=="si"){
@@ -171,9 +107,73 @@ function muestra(cantidadPrestamo, intereses, cuotas, valorCuota){
     aceptarTrato();
 }
 
-//muestra lista de los clientes
-function muestraClientes(clientes){
-    clientes.forEach((cliente) => {console.log(`Nombre: ${cliente.nombre} y ID: ${cliente.id}`)});
+function pedidoPrestamo(){
+    let cantidadPrestamo = parseFloat(prompt("¿Cuanto es el dinero que usted quiere recibir?"));
+    controlCero(cantidadPrestamo);
+    alert("¿En cuantas cuota usted quiere devolver el prestamo?");
+    let cuotas = parseInt(prompt("Las opciones son 3, 6, 12, 24 y 48 cuotas"));
+    controlCuotas(cuotas);
+    let cantidadIntereses = eleccionInteres(cuotas);
+    let intereses = calculoItereses(cantidadPrestamo, cantidadIntereses);
+    let valorFinalPrestamo = sumaDineroFinal(cantidadPrestamo, intereses);
+    let valorCuota = calculoCuota(valorFinalPrestamo, cuotas);
+    alert("Tu Prestamo a sido Calculado");
+    muestra(cantidadPrestamo, intereses, cuotas, valorCuota)
+}
+
+function menuOpciones(){
+    let menu = prompt("1. Prestamo\n2. Inversiones(Todavia no esta programado)")
+    if(menu==1){
+        pedidoPrestamo();
+    }
+}
+
+//creamos el cliente
+function crearCliente(){
+    let usuario = prompt("Introduzca un Usuario");
+    corroborarVacio(usuario);
+    let contrasenha = prompt("Introduzca su Contraseña");
+    let bloqueo = crearContrasenha(contrasenha);
+    if(bloqueo != "bloqueado34785"){//para eviar que el programa siga creando el usuario sin la contraseña no se realiza bien.
+    let edad = parseInt(prompt("Introduzca su Edad"));
+    corroborarVacio(edad);
+    let nDocumento = parseInt(prompt("Introduzca su Numero de Documento"));
+    corroborarVacio(nDocumento);
+    let salario = parseInt(prompt("Introduzca su sueldo mensual"))
+    corroborarVacio(salario);
+    usuario = new Cliente(clientes.length, usuario, edad, nDocumento, salario, contrasenha, 0);
+    alert("Usuario creado correctamente");
+    clientes.push(usuario)
+    } else {
+        alert("Cliente no Creado")
+    }
+}
+
+//funcion de iniciar sesion
+function iniciarSesion(clientes){
+    let usuarioSesion = prompt("Introduzca su usuario");
+    let usuarioEncontrado = clientes.find((cliente) => cliente.nombre.toLowerCase()==usuarioSesion.toLowerCase())
+    if(usuarioEncontrado==undefined){
+        alert("No esta registrado en nuestra base de datos.\nGracias por su Visita")
+    } else {
+        alert("Usuario Correcto");
+        corroborarContraSesion(usuarioEncontrado);//corrabora la contraseña en el inicio de sesion
+        function corroborarContraSesion(){
+            let intentosFallidos =0;
+            let contrasenhaSesion = prompt("Introduzca su Contraseña")
+            const contrasenhaOk = (contrasenhaSesion===usuarioEncontrado.contrasenha)
+            if(contrasenhaOk){
+                alert("Contraseña Correcta")
+                menuOpciones()
+            } else {
+                while(contrasenhaSesion != usuarioEncontrado.contrasenha && intentosFallidos<2){
+                    alert("Contraseña no coinciden");
+                    contrasenhaSesion = prompt("Introduzca su Contraseña");
+                    intentosFallidos++;
+                }
+            }
+        }
+    }
 }
 
 //funcion de iniciar sesion

3ffc42b67d1014ccccbd8b71a795d7080a456f42
Ivar Maximiliano Dacrema <ivar.m.dacrema@hotmail.com>
2022-09-30 10:49:34 -0300

    registro

--- js/main.js
+++ js/main.js
@@ -18,12 +18,35 @@ class Cliente{
     }
 }
 
-function obtenerListaUsuario(){
-    let clientes = JSON.parse(localStorage.getItem("clientes"))
-    if(clientes==null){
-        clientes = [
-            [1, "ivar", 123456, "ivar.m.dacrema@lalalan.com", 2634212121]
-        ]
-    }
-    return clientes;
+let ingresoUsuario = document.getElementById(`ingresoUsuario`);
+let ingresoContrasenha = document.getElementById(`ingresoContrasenha`);
+let botonInicio = document.getElementById(`botonInicio`);
+
+botonInicio.addEventListener("click", ()=>{
+    alert(botonInicio.value)
+    alert(ingresoUsuario.value)
+    alert(ingresoContrasenha.value)
+})
+
+let clientes = []
+if(localStorage.getItem(`clientes`)){
+    clientes = JSON.parse(localStorage.getItem(`clientes`))
+} else {
+    localStorage.setItem(`clientes`, JSON.stringify(clientes))
+}
+
+let botonRegistro = document.getElementById("botonRegistro")
+botonRegistro.addEventListener("click", ()=>{
+    crearUsuario(clientes)
+})
+
+function crearUsuario(clientes){
+    let usuarioInput = document.getElementById("usuarioInput")
+    let contrasenhaInput = document.getElementById("contrasenhaInput")
+    let emailInput = document.getElementById("emailInput")
+    let telefonoInput = document.getElementById("telefonoInput")
+    let nuevoCliente = new Cliente(clientes.length, usuarioInput.value, contrasenhaInput.value, emailInput.value, telefonoInput.value);
+    clientes.push(nuevoCliente)
+    localStorage.setItem(`clientes`, JSON.stringify(clientes))
+    console.log(clientes)
 }
